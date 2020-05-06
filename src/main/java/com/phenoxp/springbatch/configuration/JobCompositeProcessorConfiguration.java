2de75be566a1851b61d7e82d5db6751d67f8e13e
Achille <guemkus@yahoo.fr>
2020-05-06 07:16:51 -0400

    Composition of multiple processors actions

--- src/main/java/com/phenoxp/springbatch/configuration/JobCompositeProcessorConfiguration.java
+++ src/main/java/com/phenoxp/springbatch/configuration/JobCompositeProcessorConfiguration.java
@@ -1,26 +1,31 @@
 package com.phenoxp.springbatch.configuration;
 
 import com.phenoxp.springbatch.domain.Customer;
-import com.phenoxp.springbatch.processor.CustomerValidator;
+import com.phenoxp.springbatch.processor.FilteringItemProcessor;
+import com.phenoxp.springbatch.processor.UpperCaseItemProcessor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.database.JdbcPagingItemReader;
 import org.springframework.batch.item.file.FlatFileItemWriter;
-import org.springframework.batch.item.validator.ValidatingItemProcessor;
+import org.springframework.batch.item.support.CompositeItemProcessor;
+import org.springframework.batch.item.support.CompositeItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.phenoxp.springbatch.configuration.ConfigurationUtils.getCustomerFlatFileItemWriter;
 import static com.phenoxp.springbatch.configuration.ConfigurationUtils.getCustomerJdbcPagingItemReader;
 
-//@Configuration
-public class JobProcessorValidationConfiguration {
-
+@Configuration
+public class JobCompositeProcessorConfiguration {
     @Autowired
     private StepBuilderFactory stepBuilderFactory;
 
@@ -41,14 +46,18 @@ public class JobProcessorValidationConfiguration {
     }
 
     @Bean
-    public ValidatingItemProcessor itemProcessor() {
-        ValidatingItemProcessor<Customer> customerValidatingItemProcessor = new ValidatingItemProcessor<>(new CustomerValidator());
+    public CompositeItemProcessor<Customer, Customer> itemProcessor() throws Exception {
+        List<ItemProcessor<Customer, Customer>> deleagates = new ArrayList<>(2);
 
-        //if filter is false, when there is validationException, fail the job
-        //otherwise, continue
-        customerValidatingItemProcessor.setFilter(true);
+        deleagates.add(new FilteringItemProcessor());
+        deleagates.add(new UpperCaseItemProcessor());
 
-        return customerValidatingItemProcessor;
+        CompositeItemProcessor<Customer, Customer> compositeItemProcessor = new CompositeItemProcessor<>();
+
+        compositeItemProcessor.setDelegates(deleagates);
+        compositeItemProcessor.afterPropertiesSet();
+
+        return compositeItemProcessor;
     }
 
 
@@ -64,7 +73,7 @@ public class JobProcessorValidationConfiguration {
 
     @Bean
     public Job job() throws Exception {
-        return jobBuilderFactory.get("jobValidatingProcessor")
+        return jobBuilderFactory.get("jobCompositeProcessor")
                 .start(step1())
                 .build();
     }
